a50290e1b1ad8f48e05df1294dfc9ec84988389d
Amanda Mata <amanda.mata@evolucional.com.br>
2021-06-21 15:37:50 -0300

    testing commands and organize other files

--- PaymentContext.Tests/Entities/StudentTests.cs
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -4,7 +4,7 @@ using PaymentContext.Domain.Entities;
 using PaymentContext.Domain.Enums;
 using PaymentContext.Domain.ValueObjects;
 
-namespace PaymentContext.Tests
+namespace PaymentContext.Tests.Entities
 {
     [TestClass]
     public class StudentTests
--- PaymentContext.Tests/ValueObjects/DocumentTests.cs
+++ PaymentContext.Tests/ValueObjects/DocumentTests.cs
@@ -2,7 +2,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PaymentContext.Domain.Enums;
 using PaymentContext.Domain.ValueObjects;
 
-namespace PaymentContext.Tests
+namespace PaymentContext.Tests.ValueObjects
 {
     [TestClass]
     public class DocumentTests
